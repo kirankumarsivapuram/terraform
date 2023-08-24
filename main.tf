11ed1dbdaa3b876ef2d56dba9bf97f965a66f730
sark1 <47409849+sark1@users.noreply.github.com>
2023-08-24 17:45:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,22 @@ terraform {
       version = "~>3.0"
     }
   }
+
   
 }
-
+terraform{
+backend "azurerm" {
+    resource_group_name  = "devops"
+    storage_account_name = "filebased"
+    container_name       = "statefile"
+    key                  =  "terraform.tfstate"
+  }
+}
 provider "azurerm" {
-subscription_id  =  var.subscription_id
-tenant_id  =  var.tenant_id
-client_id  =  var.client_id
-client_secret  = var.client_secret
+#subscription_id  =  var.subscription_id
+#tenant_id  =  var.tenant_id
+#client_id  =  var.client_id
+#client_secret  = var.client_secret
   features {}
 
 }
