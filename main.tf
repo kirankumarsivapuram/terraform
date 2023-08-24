fdc47be7aa7115f4c3512c75272392bd5e353299
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-08-24 13:57:52 +0530

    Update main.tf
    added backend code to store terraform.tfstate file in storage account

--- main.tf
+++ main.tf
@@ -12,7 +12,12 @@ terraform {
       version = "~>3.0"
     }
   }
-  
+  backend "azurerm" {
+    resource_group_name  = "devops"
+    storage_account_name = "filebased"
+    container_name       = "statefile"
+    key                  =  "terraform.tfstate"
+  }
 
 
 }
