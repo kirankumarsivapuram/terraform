124cfd7587867845f226179f7e9b91fdf1f90d62
sark1 <47409849+sark1@users.noreply.github.com>
2023-08-24 18:29:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
-terraform{
 backend "azurerm" {
     resource_group_name  = "devops"
     storage_account_name = "filebased"
     container_name       = "statefile"
     key                  =  "terraform.tfstate"
   }
-}
+
