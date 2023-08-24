ece301895b71d061f7f824b57a81aed214671727
sark1 <47409849+sark1@users.noreply.github.com>
2023-08-24 17:15:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,14 +23,7 @@ client_secret  = var.client_secret
   features {}
 
 }
-terraform{
-backend "azurerm" {
-    resource_group_name  = "devops"
-    storage_account_name = "filebased"
-    container_name       = "statefile"
-    key                  =  "terraform.tfstate"
-  }
-}
+
 
 
 resource "azurerm_resource_group" "rg" {
