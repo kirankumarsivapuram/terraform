ccf3f7e9431f378b48d35c5cf255b1ea4da0d889
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-08-24 21:10:10 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "devops"
+    storage_account_name = "filebased"
+    container_name       = "statefile"
+    key                  = "terraform.tfstate"
+  }
+}
