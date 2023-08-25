b8ec7c086e01d1282af149583276b65cf9a75b8d
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-08-25 16:01:52 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-export ARM_CLIENT_ID="b26d672a-0f97-4334-98bc-fea755e1a435"
-export ARM_CLIENT_SECRET="EWN8Q~p5Rbf_sDpOKcyGWbB_BZAE_yR48Pdctclo"
-export ARM_TENANT_ID="5461eb53-e0fd-4320-92ff-c6efe1fe91e9"
-export ARM_SUBSCRIPTION_ID="e64d2b7e-f9cf-4177-afb5-b4eb654ca3cb"
+#export ARM_CLIENT_ID="b26d672a-0f97-4334-98bc-fea755e1a435"
+#export ARM_CLIENT_SECRET="EWN8Q~p5Rbf_sDpOKcyGWbB_BZAE_yR48Pdctclo"
+#export ARM_TENANT_ID="5461eb53-e0fd-4320-92ff-c6efe1fe91e9"
+#export ARM_SUBSCRIPTION_ID="e64d2b7e-f9cf-4177-afb5-b4eb654ca3cb"
