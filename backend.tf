4e5ffe108a2a8832f2dae7f284f753148c655457
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-08-28 12:15:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,20 @@
 terraform{
   backend "azurerm" {
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_tfstatefile_name
+    
+  }
+}
+
+/*
+terraform{
+  backend "azurerm" {
     resource_group_name  = "devops"
     storage_account_name = "filebased"
     container_name       = "statefile"
     key                  = "terraform.tfstate"
     
   }
-}
+}*/
